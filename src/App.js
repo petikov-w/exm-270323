9a373b08ae7db0ceda8fa732b628cfb370ddb6ca
krox2 <developer-krox@yandex.ru>
2023-03-27 23:44:22 +0300

    270323-2343 Создал компоненты Title и Flex

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
+import React from "react";
+import styled from "styled-components";
+import Flex from "./components/Flex";
+import Title from "./components/Title";
+
+const AppWrapper = styled.div`
+width: 100%;
+min-height: 100vh;
+padding: 2rem;
+background-color: silver;
+`
+
+
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <AppWrapper>
+      <Flex justify="center">
+        <Title color={"#3cff00"}>Привет, мир !!!</Title>
+      </Flex>
+    </AppWrapper>
   );
 }
 
